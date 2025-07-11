745cb6e95ea7d72c129b451ef9b4f5cf41505f95
Ashutosh Choudhary <Akkychoudhary5468@example.com>
2025-07-11 12:29:25 +0530

    updates

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Link from 'next/link';
-import { Github, Linkedin, Twitter } from 'lucide-react';
+import Link from "next/link";
+import { Github, Linkedin, FileText } from "lucide-react";
 
 export function Footer() {
   return (
@@ -17,23 +17,43 @@ export function Footer() {
         <source src="/feature-1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      
+
       <div className="relative container flex flex-col items-center justify-between gap-4 py-6 md:h-16 md:flex-row md:gap-6 md:py-0 z-10">
         <div className="flex flex-col items-center gap-2 px-4 md:flex-row md:gap-4 md:px-0">
-         
           <p className="text-sm md:text-xs leading-tight text-white text-center md:text-left">
-            Made by Ashutosh Choudhary © {new Date().getFullYear()}. All rights reserved.
+            Made by Ashutosh Choudhary © {new Date().getFullYear()}. All rights
+            reserved.
           </p>
         </div>
         <div className="flex items-center gap-4">
-          <Link href="#" target="_blank" rel="noreferrer" aria-label="GitHub" className="group">
+          <Link
+            href="https://github.com/Ashutoshchoudhary22"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="GitHub"
+            className="group"
+          >
             <Github className="h-5 w-5 md:h-4 md:w-4 text-white group-hover:text-blue-500 group-hover:scale-110 transition-all duration-300" />
           </Link>
-          <Link href="#" target="_blank" rel="noreferrer" aria-label="Twitter" className="group">
-            <Twitter className="h-5 w-5 md:h-4 md:w-4 text-white group-hover:text-blue-500 group-hover:scale-110 transition-all duration-300" />
-          </Link>
-          <Link href="#" target="_blank" rel="noreferrer" aria-label="LinkedIn" className="group">
+
+          <Link
+            href="https://www.linkedin.com/in/ashutosh-choudhary-693b1a264/"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="LinkedIn"
+            className="group"
+          >
             <Linkedin className="h-5 w-5 md:h-4 md:w-4 text-white group-hover:text-blue-500 group-hover:scale-110 transition-all duration-300" />
+          </Link>
+          <Link
+            href="/Ashutosh_Choudhary_Resume_2025.pdf"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Resume"
+            className="group"
+            download="Ashutosh_Choudhary_Resume_2025.pdf"
+          >
+            <FileText className="h-5 w-5 md:h-4 md:w-4 text-white group-hover:text-blue-500 group-hover:scale-110 transition-all duration-300" />
           </Link>
         </div>
       </div>
